a84b083ad97e2bfdbf2ec5c9dae8b99e864d6d02
vishvjeetprajapati <vishvjeetprajapati138@gmail.com>
2022-07-12 13:15:48 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+con1.innerText=null;
+function buttonPressed(id){
+let value1=document.getElementById(id).value;
+con1.innerText+=value1;
+}
+var a=0;
+var op;
+function operatorPressed(id){
+     a++;
+     var operator = document.getElementById(id).value;
+     op=operator;
+     con1.innerText+=operator;
+}
+
+buttonac.addEventListener('click',function(){
+    con1.innerText=null;
+    con2.innerText=null;
+
+})
+
+function EqualButtonPressed(){
+    try{
+     con2.innerText=eval(con1.innerText);
+    }
+    catch(error){
+        alert('Exception accured')
+    }
+}
+function reversesign(){
+    if(eval(con1.innerText)>0){
+        con1.innerText='-'+con1.innerText;
+    }
+    else{
+        con1.innerText='+'+con1.innerText;
+    }
+   
+}
